506966e5c589bb2fb767a4fe9f0f8fe7981c6592
Stefan Theard <smtheard@gmail.com>
2014-05-30 18:38:00 -0500

    updated name of index.html

---
+++ Tic Tac Toe/main.js
@@ -0,0 +1,185 @@
+
+var pos1 = document.getElementById("pos1");
+
+pos1.onclick = function() 
+{
+	//alert(pos1.src);
+	var trim = pos1.src.substring(64, pos1.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos1.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos1.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos1.src = 'empty.bmp';
+	}
+	else{
+		pos1.src = 'X.bmp';
+	}
+};
+
+var pos2 = document.getElementById("pos2");
+
+pos2.onclick = function() {
+	var trim = pos2.src.substring(64, pos2.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos2.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos2.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos2.src = 'empty.bmp';
+	}
+	else{
+		pos2.src = 'X.bmp';
+	}
+};
+
+var pos3 = document.getElementById("pos3");
+
+pos3.onclick = function() {
+	var trim = pos3.src.substring(64, pos3.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos3.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos3.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos3.src = 'empty.bmp';
+	}
+	else{
+		pos3.src = 'X.bmp';
+	}
+};
+
+var pos4 = document.getElementById("pos4");
+
+pos4.onclick = function() {
+	var trim = pos4.src.substring(64, pos4.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos4.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos4.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos4.src = 'empty.bmp';
+	}
+	else{
+		pos4.src = 'X.bmp';
+	}
+};
+
+
+var pos5 = document.getElementById("pos5");
+
+pos5.onclick = function() {
+	var trim = pos5.src.substring(64, pos5.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos5.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos5.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos5.src = 'empty.bmp';
+	}
+	else{
+		pos5.src = 'X.bmp';
+	}
+};
+
+
+var pos6 = document.getElementById("pos6");
+
+pos6.onclick = function() {
+	var trim = pos6.src.substring(64, pos6.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos6.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos6.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos6.src = 'empty.bmp';
+	}
+	else{
+		pos6.src = 'X.bmp';
+	}
+};
+
+
+var pos7 = document.getElementById("pos7");
+
+pos7.onclick = function() {
+	var trim = pos7.src.substring(64, pos7.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos7.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos7.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos7.src = 'empty.bmp';
+	}
+	else{
+		pos7.src = 'X.bmp';
+	}
+};
+
+var pos8 = document.getElementById("pos8");
+
+pos8.onclick = function() {
+	var trim = pos8.src.substring(64, pos8.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos8.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos8.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos8.src = 'empty.bmp';
+	}
+	else{
+		pos8.src = 'X.bmp';
+	}
+};
+
+var pos9 = document.getElementById("pos9");
+
+pos9.onclick = function() {
+	var trim = pos9.src.substring(64, pos9.src.length);
+	if(trim == "empty.bmp")
+	{
+		pos9.src = 'X.bmp';
+	}
+	else if(trim == "X.bmp")
+	{
+		pos9.src = 'O.bmp';
+	}
+	else if(trim == "O.bmp"){
+		pos9.src = 'empty.bmp';
+	}
+	else{
+		pos9.src = 'X.bmp';
+	}
+};
